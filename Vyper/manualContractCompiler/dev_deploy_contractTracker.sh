97ad32686e91888fa947d4bbb66b3407116783a1
LukasF1337 <lukasfrank03@gmail.com>
2024-10-28 23:03:40 +0100

    tooling

---
+++ Vyper/manualContractCompiler/dev_deploy_contractTracker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+script_dir=$( pwd )
+
+# $script_dir/contracts-compiled/combined.json
+# bytecpde from shell:
+# $ <$script_dir/contracts-compiled/combined.json jq -r '."contracts/contractTracker.vy"' | jq .bytecode
+
