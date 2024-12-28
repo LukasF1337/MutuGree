64e638b2d364ce056730e013ce8218b0515d0c8b
LukasF1337 <lukasfrank03@gmail.com>
2024-12-28 23:54:56 +0100

    contract_utils.tsx created

---
+++ app/shared_libs/contract_utils.tsx
@@ -0,0 +1,32 @@
+import smartContractsJson from '../../Vyper/manualContractCompiler/contracts-compiled/combined.json';
+import { ethers } from "ethers";
+import {
+    ContractFactory, Provider, Wallet, types, utils
+} from "zksync-ethers";
+
+
+export const contract_tracker = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
