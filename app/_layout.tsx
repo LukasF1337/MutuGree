e4ba0ee31e4d5e644911f2704bd845050a422c9d
LukasF1337 <lukasfrank03@gmail.com>
2024-12-28 23:50:43 +0100

    home button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
-import { Stack } from "expo-router";
-import { PaperProvider } from 'react-native-paper';
-import { theme } from './shared_libs/utils'
+import { Stack, Link, usePathname } from "expo-router";
+import { PaperProvider, IconButton, Text } from 'react-native-paper';
+import { theme } from './shared_libs/colors'
+import { View, StyleSheet } from "react-native";
+
+function LogoTitle() {
+  const pathname = usePathname();
+  return (
+    <PaperProvider theme={theme}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+        <View>
+          < Link href="/" asChild >
+            <IconButton icon="home-outline" iconColor={theme.colors.primary} />
+          </Link >
+        </View>
+        <View style={{ padding: 19, }}>
+          <Text style={{ color: theme.colors.primary, fontWeight: 'bold' }}>
+            {pathname}
+          </Text >
+        </View>
+      </View>
+    </PaperProvider >
+  );
+}
 
 export default function RootLayout() {
   return (
@@ -14,9 +35,15 @@ export default function RootLayout() {
           headerTitleStyle: {
             fontWeight: 'bold',
           },
+          headerTitle: props => <LogoTitle />,
+
         }}>
         {/* Optionally configure static options outside the route.*/}
-        <Stack.Screen name="index" options={{}} />
+
+        {/* <Stack.Screen name="index" options={{
+          headerTitle: props => <LogoTitle />
+        }} /> */}
+
       </Stack>
     </PaperProvider>
   );
